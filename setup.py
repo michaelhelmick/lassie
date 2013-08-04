2d1d83ead081937144fdb5f23ae95ba57cd732bc
Mike Helmick <mikeh@ydekproductions.com>
2013-08-04 00:43:30 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,6 +38,12 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Internet'
+        'Topic :: Internet',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
     ]
 )
