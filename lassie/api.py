43a459751a2a4c4edfc8c52636cb7fe644b4d4f1
Frank Sachsenheim <funkyfuture@riseup.net>
2018-05-31 01:22:06 +0200

    Removes boilerplate docs from lassie.api

--- lassie/api.py
+++ lassie/api.py
@@ -8,36 +8,18 @@ This module implements the Lassie API.
 
 """
 
-from .core import Lassie
+from lassie.core import Lassie
+
+
+lassie = Lassie()
 
 
 def fetch(url, **kwargs):
-    """Constructs and sends a :class:`Lassie <Lassie>`
-    Retrieves content from the specified url, parses it, and returns
-    a beautifully crafted dictionary of important information about that
-    web page.
+    return lassie.fetch(url, **kwargs)
 
-    Priority tree is as follows:
-        1. Open Graph
-        2. Twitter Card
-        3. Other meta content (i.e. description, keywords)
 
-    :param url: URL to send a GET request to
-    :param open_graph: (optional) If ``True``, filters web page content for Open Graph meta tags. The content of these properties have top priority on return values.
-    :type open_graph: bool
-    :param twitter_card: (optional) If ``True``, filters web page content for Twitter Card meta tags
-    :type twitter_card: bool
-    :param touch_icon: (optional) If ``True``, retrieves Apple touch icons and includes them in the response ``images`` array
-    :type touch_icon: bool
-    :param favicon: (optional) If ``True``, retrieves any favicon images and includes them in the response ``images`` array
-    :type favicon: bool
-    :param all_images: (optional) If ``True``, retrieves images inside web pages body and includes them in the response ``images`` array. Default: False
-    :type all_images: bool
-    :param parser: (optional) String reference for the parser that BeautifulSoup will use
-    :type parser: string
-    :param handle_file_content: (optional) If ``True``, lassie will return a generic response when a file is fetched. Default: False
-    :type handle_file_content: bool
+fetch.__doc__ = 'Fetches from a default :class:`Lassie` instance.\n\n' + \
+                Lassie.fetch.__doc__
 
-    """
-    l = Lassie()
-    return l.fetch(url, **kwargs)
+
+__all__ = [fetch.__name__]
